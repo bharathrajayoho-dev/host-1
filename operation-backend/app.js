661e730df1c89941452b1efe2d5c6499067ff88b
bharathrajayoho-dev <bharathraja.yoho@gmail.com>
2025-09-22 18:16:47 +0530

    hh

--- operation-backend/app.js
+++ operation-backend/app.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
     "http://localhost:5174",
     "http://mgmproperties.co.in",
     "https://mgmproperties.co.in",
+    "https://merry-dango-d632ae.netlify.app",
  
 ];
 app.use(
